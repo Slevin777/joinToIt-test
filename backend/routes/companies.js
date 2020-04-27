76c41f08112d6e23de620f28bc03b8b54edcd7b9
Vitaliy <airon223@gmail.com>
2020-04-27 14:32:48 +0300

    implement pagination

--- backend/routes/companies.js
+++ backend/routes/companies.js
@@ -10,7 +10,18 @@ const router = express.Router();
 
 //get all companies
 router.get('/', async (req, res) => {
-  const companies = await Company.find().sort('lastName').select('-__v');
+  let companies;
+  const { pageNumber, pageSize } = req.query;
+
+  if (pageNumber && pageSize) {
+    companies = await Company.find()
+      .select('-__v')
+      .skip((pageNumber - 1) * pageSize)
+      .limit(parseInt(pageSize))
+      .sort('name');
+  } else {
+    companies = await Company.find().sort('name').select('-__v');
+  }
 
   res.send(companies);
 });
@@ -31,18 +42,18 @@ router.post('/', upload.single('logo'), async (req, res) => {
 
   await company.save();
 
-  // sendMail(
-  //   'airon223@gmail.com',
-  //   'New Company added',
-  //   `Company ${req.body.name} have beed added`,
-  //   function (err, data) {
-  //     if (err) {
-  //       console.log('Sendmail error: ', err);
-  //     } else {
-  //       console.log('message sent!');
-  //     }
-  //   }
-  // );
+  sendMail(
+    'v.galchinskyy@gmail.com',
+    'New Company added',
+    `Company ${req.body.name} have beed added`,
+    function (err, data) {
+      if (err) {
+        console.log('Sendmail error: ', err);
+      } else {
+        console.log('message sent!');
+      }
+    }
+  );
 
   res.send(company);
 });
@@ -80,28 +91,25 @@ router.put('/:id', upload.single('file'), async (req, res) => {
 });
 
 //delete company by Id
-router.delete(
-  '/:id',
-  /* auth, */ async (req, res) => {
-    const company = await Company.findByIdAndRemove(req.params.id);
-    if (!company)
-      return res.status(400).send('Company with given id wan not found');
+router.delete('/:id', auth, async (req, res) => {
+  const company = await Company.findByIdAndRemove(req.params.id);
+  if (!company)
+    return res.status(400).send('Company with given id wan not found');
 
-    const fileName = company.logo.match(/(?<=uploads\\).*/);
+  const fileName = company.logo.match(/(?<=uploads\\).*/);
 
-    //delete logo if exists
-    if (fileName) {
-      fs.unlink('./uploads/' + fileName[0], (err) => {
-        if (err) {
-          console.log('failed to delete image:' + err);
-        } else {
-          console.log('image successfully deleted');
-        }
-      });
-    }
-
-    res.send(company);
+  //delete logo if exists
+  if (fileName) {
+    fs.unlink('./uploads/' + fileName[0], (err) => {
+      if (err) {
+        console.log('failed to delete image:' + err);
+      } else {
+        console.log('image successfully deleted');
+      }
+    });
   }
-);
+
+  res.send(company);
+});
 
 module.exports = router;
